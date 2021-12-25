d09d3a3e1ce986057f5263c716bd583c363a1e38
Amirrr1987 <Maghami.A1987@gmail.com>
2021-12-26 00:48:18 +0330

    New(MovieCard)
    - create MovieCard component
    - add style and tailwind class
    - export IconCalendar form Figma and change vue component width color and size props
    - create card width ratio : 16 / 9
    - use tailwind jit mode for color and grid cols

---
+++ src/components/IconCalendar.vue
@@ -0,0 +1,45 @@
+<template>
+  <svg
+    width="16"
+    height="16"
+    viewBox="0 0 16 16"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    :width="size"
+    :height="size"
+    :fill="color"
+  >
+    <rect width="16" height="16" fill="url(#pattern0)" />
+    <defs>
+      <pattern
+        id="pattern0"
+        patternContentUnits="objectBoundingBox"
+        width="1"
+        height="1"
+      >
+        <use
+          xlink:href="#image0_8_18"
+          transform="translate(0 -0.015625) scale(0.03125)"
+        />
+      </pattern>
+      <image
+        id="image0_8_18"
+        width="32"
+        height="33"
+        xlink:href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAhCAQAAAASL2HaAAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAAmJLR0QA/4ePzL8AAAAHdElNRQfiAwoOOCiX8bOWAAAA8ElEQVRIx+2VTw7BUBCHv9afBVsShxHHEAcQcQaLLiyqV+AGIjiDOAzClkVbxkbrTUkpEpv+ZtHk9958b+a9ZAr/lvXgtGkBa6YJv0MTWDFLBzrILYbKH8a+kw7YIHi4CAfl7xFcPIRtOiBEKABnxGjPQjgDBYRQJxRv3yp96oBtrI2eHmHjAVsmnEx7Efd4r0BHVEEUC809ZgYcdQslA1YnfPK8FrV4t5ERbfQV4h0FlElc2kcyixpweTPLxk1aPqJgr44V/Ij1pXJADsgBPwIU4/lSopthHkCgreXDEH0Vcz0TK/RoZKh8x1j/F/6nK0D2d+Xdbd9cAAAAJXRFWHRkYXRlOmNyZWF0ZQAyMDE4LTAzLTEwVDE0OjU2OjQwKzAwOjAwQn9tOgAAACV0RVh0ZGF0ZTptb2RpZnkAMjAxOC0wMy0xMFQxNDo1Njo0MCswMDowMDMi1YYAAAAASUVORK5CYII="
+      />
+    </defs>
+  </svg>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "TheApp",
+  props: {
+    color: { type: String, default: "#000" },
+    size: { type: String, default: "16" },
+  },
+});
+</script>
---
+++ src/components/MovieCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <figure class="movie-card">
+    <img class="h-full" src="@/assets/img/cover.png" title="" alt="" />
+    <figcaption class="px-1 py-5 flex flex-col justify-between">
+      <h4 class="font-bold">Inception</h4>
+      <div class="text-sm">
+        <div class="flex items-center mb-3">
+          <IconCalendar class="mr-3" />
+          <span>2014-12-01</span>
+        </div>
+        <ul class="list-disc flex justify-around">
+          <li class="">Action</li>
+          <li class="">Adventure</li>
+          <li class="">Sci-Fi</li>
+        </ul>
+      </div>
+    </figcaption>
+  </figure>
+</template>
+<script lang="ts" setup>
+import IconCalendar from "./IconCalendar.vue";
+</script>
+<style>
+.movie-card {
+  @apply bg-[#F1F1F1] grid grid-cols-[43%,57%] aspect-video w-full rounded shadow drop-shadow overflow-hidden p-1 max-w-[500px] mx-auto;
+}
+</style>
