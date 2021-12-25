931ff868d39b44d97dfaad8ddbd0e8cb7b4e873c
Amirrr1987 <Maghami.A1987@gmail.com>
2021-12-26 00:53:14 +0330

    New(import components)
    - import SearchBar component on top
    - import MovieCard components sample
    - fix style and layouts size

--- src/pages/user/TheUserHome.vue
+++ src/pages/user/TheUserHome.vue
@@ -1,8 +1,37 @@
 <template>
-    <div class="the-home">
-        TheHome.vue
-    </div>
+  <div class="the-home">
+    <section class="py-10">
+      <div class="container mx-auto">
+        <SearchBar />
+      </div>
+    </section>
+    <section>
+      <div class="container mx-auto">
+        <div
+          class="grid md:grid-cols-2 lg:grid-cols-3 gap-5 md:gap-10 lg:gap-12"
+        >
+          <MovieCard />
+          <MovieCard />
+          <MovieCard />
+          <MovieCard />
+          <MovieCard />
+          <MovieCard />
+          <MovieCard />
+          <MovieCard />
+        </div>
+      </div>
+    </section>
+    <section class="py-5 lg:py-10">
+      <div class="flex justify-center">
+        <button class="text-gray-500 capitalize">previous page</button>
+        <div class="h-[50px] bg-gray-400 w-[2px] mx-3"></div>
+        <button class="text-blue-500 capitalize">next page</button>
+      </div>
+      <div class="text-center">Showing results 1-20</div>
+    </section>
+  </div>
 </template>
 <script lang="ts" setup>
-
+import MovieCard from "./../../components/MovieCard.vue";
+import SearchBar from "../../components/SearchBar.vue";
 </script>
