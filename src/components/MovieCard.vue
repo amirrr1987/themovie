02922802362144dda566ff2fb2f83d145359324c
Amirrr1987 <Maghami.A1987@gmail.com>
2021-12-26 23:14:00 +0330

    Fix(responsive)
    - fix size for responsive
    - change ul width div and span
    - ...

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -1,18 +1,20 @@
 <template>
   <figure class="movie-card">
     <img class="h-full" src="@/assets/img/cover.png" title="" alt="" />
-    <figcaption class="px-1 py-5 flex flex-col justify-between">
+    <figcaption class="px-1 py-3 flex flex-col justify-between">
       <h4 class="font-bold">Inception</h4>
       <div class="text-sm">
         <div class="flex items-center mb-3">
           <IconCalendar class="mr-3" />
           <span>2014-12-01</span>
         </div>
-        <ul class="list-disc flex justify-around">
-          <li class="">Action</li>
-          <li class="">Adventure</li>
-          <li class="">Sci-Fi</li>
-        </ul>
+        <div class="inline-flex justify-around items-center">
+          <span class="">Action</span>
+          <i class="bg-gray-500 w-[6px] h-[6px] rounded-full mx-1"></i>
+          <span class="">Adventure</span>
+          <i class="bg-gray-500 w-[6px] h-[6px] rounded-full mx-1"></i>
+          <span class="">Sci-Fi</span>
+        </div>
       </div>
     </figcaption>
   </figure>
@@ -22,6 +24,6 @@ import IconCalendar from "./IconCalendar.vue";
 </script>
 <style>
 .movie-card {
-  @apply bg-[#F1F1F1] grid grid-cols-[43%,57%] aspect-video w-full rounded shadow drop-shadow overflow-hidden p-1 max-w-[500px] mx-auto;
+  @apply bg-[#F1F1F1] grid grid-cols-[max-content,1fr] gap-x-3 w-full rounded shadow drop-shadow overflow-hidden p-1 mx-auto;
 }
 </style>
