be6b7b8cb563048fc3d2e8d3f5e0a2b4519e839d
Amirrr1987 <Maghami.A1987@gmail.com>
2021-12-26 23:12:51 +0330

    Fix(style)
    - fix responsive and size and margin and padding

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,16 +1,31 @@
 <template>
   <form class="search-bar">
     <div class="">
-      <label class="mr-5">Search by release date:</label>
-      <input type="text" class="px-3 py-1 outline-none" />
+      <label class="hidden md:inline mr-5">Search by release date:</label>
+      <input
+        type="text"
+        class="
+          w-full
+          md:w-max
+          mb-5
+          md:mb-0
+          px-3
+          py-2
+          md:py-1
+          rounded
+          outline-none
+        "
+      />
     </div>
     <button
       class="
         bg-blue-500
         focus:bg-blue-700
         hover:bg-blue-300
-        px-5
+        md:px-10
         py-2
+        w-full
+        md:w-max
         rounded-full
         text-white
       "
@@ -22,6 +37,6 @@
 <script lang="ts" setup></script>
 <style>
 .search-bar {
-  @apply bg-[#E2E2E2] px-16 py-10 rounded shadow drop-shadow flex justify-between;
+  @apply bg-[#E2E2E2] px-16 py-10 rounded shadow drop-shadow md:flex md:justify-between;
 }
 </style>
