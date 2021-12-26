2eb274ccb24aede52ed91973b24a89441475ccac
Amirrr1987 <Maghami.A1987@gmail.com>
2021-12-27 03:23:56 +0330

    Feat(store)
    - set and get Movies method in reactive store
    -  getMovieItem method to get movie items
    - return store in App.vue to see in vue devtools

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
+<div class="hidden">{{store}}</div>
   <router-view />
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import store from './services/store';
+</script>
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  /* font-family: Avenir, Helvetica, Arial, sans-serif; */
+  /* -webkit-font-smoothing: antialiased; */
+  /* -moz-osx-font-smoothing: grayscale; */
   color: #2c3e50;
+  
 }
 </style>
--- src/services/store.ts
+++ src/services/store.ts
@@ -1,5 +1,20 @@
 import { reactive, readonly } from "vue";
 
-const state = reactive({});
+const state = reactive({
+  movies: [] ?? null,
+});
+export const setMovies = (list: any): void => {
+  state.movies = state.movies.concat(list);
+};
+export const getMovies = (): Object => {
+  return state.movies;
+};
+
+export const getMovieItem = (id: any) => {
+  let data = state.movies.filter((item: object) => {
+    return item.id == id;
+  });
+    return data
+};
 
 export default { state: readonly(state) };
