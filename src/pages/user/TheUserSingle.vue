b7ec87287ac16e024d5f0ee1276f45edc6bd303a
Amirrr1987 <Maghami.A1987@gmail.com>
2021-12-27 03:29:31 +0330

    Feat(Movie Single Item)
    - get id from params
    - call getMovieItem method from store
    - bind data in template

--- src/pages/user/TheUserSingle.vue
+++ src/pages/user/TheUserSingle.vue
@@ -2,61 +2,96 @@
   <div class="the-user-single py-5">
     <div class="container mx-auto px-5">
       <div class="back-bar bg-[#E2E2E2] rounded">
-        <button class="bg-[#549DF2] capitalize px-10 py-2 rounded-full text-white mr-10">back</button>
+        <router-link
+          class="
+            bg-[#549DF2]
+            capitalize
+            self-center
+            px-10
+            py-2
+            rounded-full
+            text-white
+            mr-10
+          "
+          to="/"
+        >
+          back
+        </router-link>
         <div class="flex flex-col">
-            <strong class="">Guardians of the Galaxy</strong>
-            <span>All heroes start somewhere.</span>
+          <strong class="">{{ moveItem[0].title }}</strong>
+          <span>{{ moveItem[0].original_title }}</span>
         </div>
       </div>
       <div class="py-5">
-          <div class="grid grid-cols-[max-content,1fr] gap-x-10 mb-5">
-          <img class="max-w-full" src="@/assets/img/cover-single.png" alt="">
+        <div class="grid grid-cols-[max-content,1fr] gap-x-10 mb-5">
+          <img
+            class="max-w-full rounded"
+            :src="`https://image.tmdb.org/t/p/w400${moveItem[0].poster_path}`"
+            alt=""
+          />
           <div class="">
-              <div class="flex justify-between mb-2">
-                  <span>Budget</span>
-                  <span>$170’000’000</span>
-              </div>
-              <div class="flex justify-between mb-2">
-                  <span>Revenue</span>
-                  <span>$772’776’600</span>
-              </div>
-              <div class="flex justify-between mb-2">
-                  <span>Release Date</span>
-                  <span>2014-07-30</span>
-              </div>
-              <div class="flex justify-between mb-2">
-                  <span>Runtime</span>
-                  <span>2h 1m</span>
-              </div>
-              <div class="flex justify-between mb-2">
-                  <span>Score</span>
-                  <span>7.9 (23563 votes)</span>
-              </div>
-              <div class="flex justify-between mb-2">
-                  <span>Genres</span>
-                  <span>Action, Science Fiction, Adventure</span>
-              </div>
-              <div class="flex justify-between mb-2">
-                  <span>IMDB Link</span>
-                  <a href="#">Link</a>
-              </div>
-              <div class="flex justify-between mb-2">
-                  <span>Homepage Link</span>
-                  <a href="#">Link</a>
-              </div>
+            <div class="flex justify-between mb-2">
+              <span>Budget</span>
+              <span>$170’000’000</span>
+            </div>
+            <div class="flex justify-between mb-2">
+              <span>Revenue</span>
+              <span>$772’776’600</span>
+            </div>
+            <div class="flex justify-between mb-2">
+              <span>Release Date</span>
+              <span>2014-07-30</span>
+            </div>
+            <div class="flex justify-between mb-2">
+              <span>Runtime</span>
+              <span>2h 1m</span>
+            </div>
+            <div class="flex justify-between mb-2">
+              <span>Score</span>
+              <span>7.9 (23563 votes)</span>
+            </div>
+            <div class="flex justify-between mb-2">
+              <span>Genres</span>
+              <span>Action, Science Fiction, Adventure</span>
+            </div>
+            <div class="flex justify-between mb-2">
+              <span>IMDB Link</span>
+              <a href="#">Link</a>
+            </div>
+            <div class="flex justify-between mb-2">
+              <span>Homepage Link</span>
+              <a href="#">Link</a>
+            </div>
           </div>
-          </div>
-          <p class="mb-5">
-              Light years from Earth, 26 years after being abducted, Peter Quill finds himself the prime target of a manhunt after discovering an orb wanted by Ronan the Accuser. Light years from Earth, 26 years after being abducted, Peter Quill finds himself the prime target of a manhunt after discovering an orb wanted by Ronan the Accuser. Light years from Earth, 26 years after being abducted, Peter Quill finds himself the prime target of a manhunt after discovering an orb wanted by Ronan the Accuser.
-          </p>
-          <strong class="mb-2 block">Credit:</strong>
-          <p>
-              Zoe Saldanna , Vin Diesel , Chris Pratt , Bradley Cooper , Lee Pace , Zoe Saldanna , Vin Diesel , Chris Pratt , Bradley Cooper , Lee Pace and 19 more.
-          </p>
+        </div>
+        <p class="mb-5">
+          {{ moveItem[0].overview }}
+        </p>
+        <strong class="mb-2 block">Credit:</strong>
+        <p>
+          Zoe Saldanna , Vin Diesel , Chris Pratt , Bradley Cooper , Lee Pace ,
+          Zoe Saldanna , Vin Diesel , Chris Pratt , Bradley Cooper , Lee Pace
+          and 19 more.
+        </p>
       </div>
     </div>
   </div>
 </template>
+<script lang="ts" setup>
+import { computed, ref } from "vue";
+import { useRoute } from "vue-router";
+import { getMovieItem } from "../../services/store";
+
+const route = useRoute();
+const movieId = ref("");
+
+movieId.value = `${route.params.id}`;
+
+const moveItem = computed(() => {
+  return getMovieItem(movieId.value);
+});
+console.log(moveItem.value[0]);
+</script>
 <style>
 .back-bar {
   @apply bg-[#E2E2E2] px-16 py-8 rounded shadow drop-shadow md:flex;
