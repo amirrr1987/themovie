d2a2385e2efc9fbe8868ccb26cecbfab84334f4b
Amirrr1987 <Maghami.A1987@gmail.com>
2021-12-28 01:38:23 +0330

    fix: api variables
    - set api key variable ad use in methods
    - set page in query

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,5 @@
 import Axios from "axios";
+let apiKey = "f62f750b70a8ef11dad44670cfb6aa57";
 const DATA = Axios.create({
   baseURL: "https://api.themoviedb.org/3/",
   headers: {
@@ -7,12 +8,12 @@ const DATA = Axios.create({
   },
 });
 
-export const GetMovieList = () => {
-  return DATA.get("discover/movie?api_key=f62f750b70a8ef11dad44670cfb6aa57");
+export const GetMovieList = (page: string) => {
+  return DATA.get(
+    `discover/movie?api_key=${apiKey}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_watch_monetization_types=flatrate`
+  );
 };
 
 export const GetMovieDetails = (movieId: string) => {
-  return DATA.get(
-    `movie/${movieId}?api_key=f62f750b70a8ef11dad44670cfb6aa57`
-  );
+  return DATA.get(`movie/${movieId}?api_key=${apiKey}`);
 };
