cf8a98505eb962c296c4dd9de565bf5b22087aea
Amir Maghami <maghami.a1987@gmail.com>
2021-12-28 10:42:49 +0330

    feat: install Nprogress package
    - install Nprogress package
    - create Nprogress styles in assets folder
    - import Nprogress css file in main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router';
 import './assets/css/tailwind.css'
+import "./assets/css/nprogress.css";
+// import "vue-transitions-css";
+
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
