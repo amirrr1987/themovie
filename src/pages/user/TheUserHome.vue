44c6b0ed55e2ec54e960a8af767b99f6f6671b3f
Amirrr1987 <Maghami.A1987@gmail.com>
2021-12-30 19:45:51 +0330

    fix: genre
    - fix genre
    - feat date picker
    - feat genresList

--- src/pages/user/TheUserHome.vue
+++ src/pages/user/TheUserHome.vue
@@ -3,6 +3,56 @@
     <section class="py-10">
       <div class="container mx-auto px-5">
         <SearchBar />
+        <div class="shadow drop-shadow my-1 py-3 flex">
+          <button
+            class="
+              mx-5
+              bg-blue-500
+              px-5
+              py-1
+              text-white
+              rounded
+              capitalize
+              flex
+              items-center
+            "
+            @click="popularityOrder"
+          >
+            popularity
+            <i
+              class="text-xs"
+              :class="
+                popularity == 'desc' ? 'icon--sort-down' : 'icon--sort-up'
+              "
+            ></i>
+          </button>
+          <form class="flex" @submit.prevent="searchByDate">
+            <div class="mx-3 capitalize">
+              <label for="start" class="mr-2">start date:</label>
+              <input
+                name="start"
+                type="date"
+                class="border px-3 py-1"
+                v-model="startDate"
+              />
+            </div>
+            <div class="mr-5">
+              <label for="end" class="mr-2">end date:</label>
+              <input
+                name="edn"
+                type="date"
+                class="border px-3 py-1"
+                v-model="endDate"
+              />
+            </div>
+            <button
+              type="submit"
+              class="bg-blue-500 text-blue-50 px-5 py-1 rounded-full"
+            >
+              Search Date
+            </button>
+          </form>
+        </div>
       </div>
     </section>
     <section>
@@ -15,6 +65,7 @@
               :title="item.title"
               :date="item.release_date"
               :imgSrc="item.poster_path"
+              :genres="item.genres"
               @click="getMovieItem(item.id)"
             />
           </template>
@@ -40,8 +91,8 @@
 <script lang="ts" setup>
 import MovieCard from "./../../components/MovieCard.vue";
 import SearchBar from "../../components/SearchBar.vue";
-import { computed, onMounted, ref } from "vue";
-import { GetMovieList } from "../../services/api";
+import { computed, onBeforeMount, onMounted, ref } from "vue";
+import { GetGenres, GetMovieList } from "../../services/api";
 import { useRoute, useRouter } from "vue-router";
 import NProgress from "NProgress";
 const movieList = ref();
@@ -60,15 +111,33 @@ const currentPage = computed({
   },
 });
 
+const genresList = ref();
+onBeforeMount(async () => {
+  try {
+    let { data } = await GetGenres();
+    genresList.value = data.genres;
+  } catch (error) {
+    throw error;
+  }
+});
+
 const startItem = ref(1);
 const endItem = ref(20);
 
 onMounted(async () => {
   NProgress.start();
   try {
-    let { data } = await GetMovieList(`${currentPage.value}`);
-    console.log(data);
+    let { data } = await GetMovieList(
+      `${currentPage.value}`,
+      "popularity.desc"
+    );
     movieList.value = data.results;
+    movieList.value.forEach((item: any) => {
+      item.genres = genresList.value
+        .filter((genre: any) => item.genre_ids.includes(genre.id))
+        .map((item: any) => item.name);
+    });
+    console.log(movieList.value);
     startItem.value = data.page * data.results.length + 1 - data.results.length;
     endItem.value = startItem.value + (data.results.length - 1);
     router.push({ name: "TheUserHome", query: { page: data.page } });
@@ -92,6 +161,12 @@ const nextPageHandel = async () => {
     let { data } = await GetMovieList(`${Number(currentPage.value) + 1}`);
     console.log(data);
     movieList.value = data.results;
+    movieList.value.forEach((item: any) => {
+      item.genres = genresList.value
+        .filter((genre: any) => item.genre_ids.includes(genre.id))
+        .map((item: any) => item.name);
+    });
+    console.log(movieList.value);
     startItem.value = data.page * data.results.length + 1 - data.results.length;
     endItem.value = startItem.value + (data.results.length - 1);
     router.push({ name: "TheUserHome", query: { page: data.page } });
@@ -109,6 +184,12 @@ const previousPageHandel = async () => {
     let { data } = await GetMovieList(`${Number(currentPage.value) - 1}`);
     console.log(data);
     movieList.value = data.results;
+    movieList.value.forEach((item: any) => {
+      item.genres = genresList.value
+        .filter((genre: any) => item.genre_ids.includes(genre.id))
+        .map((item: any) => item.name);
+    });
+    console.log(movieList.value);
     startItem.value = data.page * data.results.length + 1 - data.results.length;
     endItem.value = startItem.value + (data.results.length - 1);
     router.push({ name: "TheUserHome", query: { page: data.page } });
@@ -117,6 +198,87 @@ const previousPageHandel = async () => {
     throw error;
   } finally {
     NProgress.done();
+  }
+};
+
+///////////////////////////////////////////////
+// Start: popularity Order
+/**
+ * This is function.
+ * @param {string} popularity - A string param
+ * @return {array} A good string
+ *
+ * ```
+ */
+const popularity = ref("desc");
+const popularityOrder = async () => {
+  popularity.value = popularity.value == "desc" ? "asc" : "desc";
+  try {
+    let { data } = await GetMovieList(`1`, popularity.value);
+    console.log(data);
+    movieList.value = data.results;
+    movieList.value.forEach((item: any) => {
+      item.genres = genresList.value
+        .filter((genre: any) => item.genre_ids.includes(genre.id))
+        .map((item: any) => item.name);
+    });
+    console.log(movieList.value);
+    startItem.value = data.page * data.results.length + 1 - data.results.length;
+    endItem.value = startItem.value + (data.results.length - 1);
+    router.push({ name: "TheUserHome", query: { page: data.page } });
+    window.scrollTo(0, 0);
+  } catch (error) {
+    throw error;
+  } finally {
+    NProgress.done();
+  }
+};
+
+// End: popularity Order
+///////////////////////////////////////////////
+
+// ///////////////////////////////////////////////
+// // Start: popularity Order
+// /**
+//  * This is function.
+//  * @param {string} popularity - A string param
+//  * @return {array} A good string
+//  *
+//  * ```
+//  */
+// const releaseYear = ref("primary_release_year");
+// const popularityOrder = async () => {
+//   popularity.value = popularity.value == "desc" ? "asc" : "desc";
+//   try {
+//     let { data } = await GetMovieList(`1`, popularity.value);
+//     console.log(data);
+//     movieList.value = data.results;
+//     startItem.value = data.page * data.results.length + 1 - data.results.length;
+//     endItem.value = startItem.value + (data.results.length - 1);
+//     router.push({ name: "TheUserHome", query: { page: data.page } });
+//     window.scrollTo(0, 0);
+//   } catch (error) {
+//     throw error;
+//   } finally {
+//     NProgress.done();
+//   }
+// };
+
+// // End: popularity Order
+///////////////////////////////////////////////
+
+const startDate = ref("");
+const endDate = ref("");
+
+const searchByDate = async () => {
+  try {
+    let { data } = await GetMovieList(
+      "1",
+      `release_date.gte=${startDate.value}&release_date.lte=${endDate.value}`
+    );
+    console.log(data);
+  } catch (error) {
+    throw error;
   }
 };
 </script>
