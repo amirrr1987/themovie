b27b2c4d0184d6b66abaa8e4b76e4fb6d83ba301
Amir Maghami <Maghami.A1987@gmail.com>
2023-06-04 20:06:58 +0430

    feat: update

--- src/stores/discover.ts
+++ src/stores/discover.ts
@@ -1,7 +1,7 @@
 import { reactive } from 'vue'
 import { defineStore } from 'pinia'
 import { getDiscoverApi } from '@/services/discover'
-import { cloneDeep, assign } from 'lodash-es'
+import { cloneDeep, assign } from 'lodash'
 import type { State } from '@/models/discover'
 import { _init } from '@/init/discover'
 import { clg } from '@/utils'
