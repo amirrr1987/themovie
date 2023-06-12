2642e4978c8e07643c0a2abd49194a4cb130e7d0
Amir Maghami <Maghami.A1987@gmail.com>
2023-06-12 09:53:46 +0430

    feat: antd vue and unocss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import Components from 'unplugin-vue-components/vite';
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
+
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    '@unocss/nuxt',
+  ],
+  css: [
+    'ant-design-vue/dist/antd.css',
+  ],
+  vite: {
+    ssr: {
+      noExternal: ['ant-design-vue',],
+    },  
+  },
 })
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="p-4 grid grid-cols-4 gap-4">
+    <Card v-for="(item, index) in 4">{{ item }}{{ index }}</Card>
+  </div>
+</template>
+<script setup lang="ts">
+import { Card } from "ant-design-vue";
+</script>
+<style></style>
